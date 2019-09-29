1870960e556310239d38950f221f6288921d6548
mdjunaid14 <mdjunaid14@outlook.com>
2019-09-29 17:07:46 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store";
 
 export default class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <Provider store={store}>
+        <div className="App"></div>;
+      </Provider>
+    );
   }
 }
