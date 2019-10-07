891ba58a2caa93542ed28436f1980ccecf551038
mdjunaid14 <mdjunaid14@outlook.com>
2019-10-08 00:34:53 +0530

    Version 1.0.6

--- src/components/Layouts/Thank.js
+++ src/components/Layouts/Thank.js
@@ -3,7 +3,10 @@ import React, { Component } from "react";
 export default class Thank extends Component {
   render() {
     return (
-      <div className="h1 dimensions">
+      <div className="h1 dimensions text-center align-middle">
+        <br />
+        <br />
+        <br />
         <br />
         Thank You for using our app :)
       </div>
