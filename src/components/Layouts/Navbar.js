70d9c63c8c739dce67dbb2542916be1f6fdd901f
mdjunaid14 <mdjunaid14@outlook.com>
2019-10-07 19:30:39 +0530

    Version 1.0.3

--- src/components/Layouts/Navbar.js
+++ src/components/Layouts/Navbar.js
@@ -4,9 +4,22 @@ export default class Navbar extends Component {
   render() {
     return (
       <>
-        <nav className="navbar navbar-lg bg-dark mb-3 fixed-top navbar-changes">
-          <div className="navbar-brand mb-0 h1 mx-auto">
+        <nav className="navbar navbar-lg bg-light mb-3 fixed-top navbar-changes">
+          <div className="navbar-brand mb-0 h1 mr-auto">
             Comprehensive Income Tax Calculator
+          </div>
+          <div className="mb-0 h6 ml-auto">
+            <a
+              href="https://www.linkedin.com/in/mdjunaid14/"
+              className="link-changes"
+            >
+              <i className="fab fa-linkedin"></i>
+              {/* My LinkedIn Profile */}
+            </a>{" "}
+            <a href="https://github.com/mdjunaid14/" className="link-changes">
+              <i class="fab fa-github"></i>
+              {/* My Github Profile */}
+            </a>
           </div>
         </nav>
       </>
